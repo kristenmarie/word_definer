d61093ca68d4541427bc636d378943501a3dac2e
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-09-15 09:18:40 -0700

    complete basic layout page and shell of app.rb

---
+++ app.rb
@@ -0,0 +1,31 @@
+require('sinatra')
+require('sinatra/reloader')
+require('pry')
+also_reload('lib/**/*.rb')
+require('./lib/word_definer')
+
+
+get('/')do
+  @words = Word.all()
+  erb(:index)
+end
+
+post('/') do
+  word = params["word"]
+  new_word = Word.new({:word => word})
+  word.save()
+  @words = Word.all()
+end
+
+get('/words/:word') do
+  @word = word.find_word(params[:word])
+  erb(:definitions)
+end
+
+post('/words/:word') do
+  word = params["last_name"]
+  @contact = Contact.find_by_last_name(name)
+  new_phone_number = params["new_phone_number"]
+  @contact.add_phone_number(new_phone_number)
+  erb(:definitions)
+end
